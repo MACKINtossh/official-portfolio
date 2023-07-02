32d22b19a7f9a4100581131fbfab2b7b2a7b7c30
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-02 10:18:36 -0500

    added the gateway picture

--- src/assets/index.js
+++ src/assets/index.js
@@ -28,7 +28,7 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
+import gateway from "./gateway.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
@@ -60,7 +60,7 @@ export {
   shopify,
   starbucks,
   tesla,
-  carrent,
+  gateway,
   jobit,
   tripguide,
   tiger,
